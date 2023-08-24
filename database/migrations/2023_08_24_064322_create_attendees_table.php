e0c7ce8ce2d8df604f2ba7c0fbb6225933eeed62
Jeremy <jabulencia@yondu.com>
2023-08-24 14:59:39 +0800

    migrations and relationships

--- database/migrations/2023_08_24_064322_create_attendees_table.php
+++ database/migrations/2023_08_24_064322_create_attendees_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
+
             $table->timestamps();
         });
     }
