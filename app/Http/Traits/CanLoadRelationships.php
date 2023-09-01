24495a766381dc11280e7d9a5fb93da2de24caef
Jeremy <jabulencia@yondu.com>
2023-09-01 09:53:22 +0800

    Loading Scoped Relationship

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -5,13 +5,14 @@ namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
     public function loadRelationships(
-        Model|EloquentBuilder|QueryBuilder $for,
+        Model|EloquentBuilder|QueryBuilder|HasMany $for,
         ?array $relations = null
-    ): Model|EloquentBuilder|QueryBuilder
+    ): Model|EloquentBuilder|QueryBuilder|HasMany
     {
         $relations = $relations ?? $this->relations ?? [];
         foreach ($relations as $relation) {
